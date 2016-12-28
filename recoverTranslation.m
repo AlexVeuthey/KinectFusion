3edc00729224be9190b0415558dfd53083f1ec80
Alex Veuthey <alexandre.veuthey@epfl.ch>
2016-12-28 18:04:20 +0100

    Implemented manual 3D translation recovery

--- recoverTranslation.m
+++ recoverTranslation.m
@@ -1,14 +1,46 @@
-imL = imread('data/sl_keyboard/markers_images/keyboard1.jpg');
-imR = imread('data/sl_keyboard/markers_images/keyboard2.jpg');
+% read the 3 translation images
+im1 = imread('data/sl_keyboard/markers_images/keyboard1.jpg');
+im2 = imread('data/sl_keyboard/markers_images/keyboard2.jpg');
+im3 = imread('data/sl_keyboard/markers_images/keyboard3.jpg');
 
+% load the offset values
+load('X_proj_keyboard_1.mat');
+load('X_proj_keyboard_2.mat');
+load('X_proj_keyboard_3.mat');
+
+% select what translation we want to work with
+imL = im1;
+imR = im2;
+rectL = rect_keyboard_1;
+rectR = rect_keyboard_2;
+pointsL = X_proj_keyboard_1;
+pointsR = X_proj_keyboard_2;
+
+%get a point on the first image
 f1 = figure(1);
 imshow(imL);
 [xL, yL] = getpts(f1);
 close;
+xL = round(xL);
+yL = round(yL);
 
+% get the corresponding point in the second image
 f2 = figure(2);
 imshow(imR);
 [xR, yR] = getpts(f2);
 close;
+xR = round(xR);
+yR = round(yR);
 
-vector = [xR-xL, yR-xL];
+clear f1 f2;
+
+% find the indexes of real-world CS points by offsetting the points
+idxL = [xL - rectL(1), yL - rectL(2)];
+idxR = [xR - rectR(1), yR - rectR(2)];
+
+% find the real-world CS 3D location for both points and get the
+% translation vector from them
+posL = pointsL(idxL(1), idxL(2), :);
+posR = pointsR(idxR(1), idxR(2), :);
+vector = [posL(1) - posR(1), posL(2) - posR(2), posL(3) - posR(3)];
+
