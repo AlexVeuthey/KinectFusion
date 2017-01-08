2e775f89041dc1e6d3a8af1674c91a4dd7f6751e
Alex Veuthey <alexandre.veuthey@epfl.ch>
2017-01-08 16:04:26 +0100

    Wrote a bit of Results, added simple gridStep argument to fusion

--- alignment/fuse_viewpoints.m
+++ alignment/fuse_viewpoints.m
@@ -1,9 +1,13 @@
-function [ transform, fused_pc ] = fuse_viewpoints( moving, fixed, merge )
+function [ transform, fused_pc ] = fuse_viewpoints( moving, fixed, merge, gridStep )
 %FUSE_VIEWPOINTS Simple wrapper for pcregrigid with automatic merging
 %   The inlier ratio is very important for a correct fusion! 0.5 to 0.8
 %   gives good results.
 %   The third argument is meant for selection between trivial fusion or
 %   using pcmerge from matlab (with box grid filter).
+
+if nargin < 4
+    gridStep = 0.01;
+end
 
 movingD = pcdownsample(moving, 'gridAverage', 1);
 fixedD = pcdownsample(fixed, 'gridAverage', 1);
@@ -13,7 +17,7 @@ transform = pcregrigid(movingD, fixedD, 'Metric', 'pointToPlane', 'InlierRatio',
 movingAligned = pctransform(moving, transform);
 
 if merge == 1
-    fused_pc = pcmerge(fixed, movingAligned, 0.001);
+    fused_pc = pcmerge(fixed, movingAligned, gridStep);
 elseif merge == 0
     fused_pc = fuse_pc(fixed, movingAligned);
 end
